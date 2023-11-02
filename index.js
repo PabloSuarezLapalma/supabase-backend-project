0119d39c1c7318b33f93b289e337afa101d58e94
Pablo Suárez Lapalma <84790504+PabloSuarezLapalma@users.noreply.github.com>
2023-11-02 12:08:13 -0300

    Data Retrieve Functional

---
+++ index.js
@@ -0,0 +1,12 @@
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
+
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3YXRoZHFwdnh1bXR4d3Jtd2d3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODg4OTgyNSwiZXhwIjoyMDE0NDY1ODI1fQ.ywoRnpwVfs4deMNdkQ0I5cWjShsUJexQ0ngaKFgSkrY'
+const SUPABASE_URL = 'https://ewathdqpvxumtxwrmwgw.supabase.co'
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY )
+
+
+let { data: Clientes, error } = await supabase
+    .from('Clientes')
+    .select('*')
+
+console.log( Clientes[0].id , Clientes[0].created_at)
